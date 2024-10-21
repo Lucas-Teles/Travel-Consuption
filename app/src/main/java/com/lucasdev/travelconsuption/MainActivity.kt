3ae61e7a63395e16287f3e1e8c5289e9498cd193
Lucas Teles <lucasouzateles@gmail.com>
2024-10-21 17:41:54 -0300

    feat: adicionando lógica e validações

--- app/src/main/java/com/lucasdev/travelconsuption/MainActivity.kt
+++ app/src/main/java/com/lucasdev/travelconsuption/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.lucasdev.travelconsuption
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.lucasdev.travelconsuption.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_Calculate) {
+            calculate()
         }
     }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != "")
+    }
+
+    private fun calculate() {
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+            if (autonomy == 0f) {
+                Toast.makeText(this, R.string.no_div_zero, Toast.LENGTH_SHORT).show()
+                binding.labelRes.text = "R$ 0"
+            } else {
+                val totalValue = (price * distance) / autonomy
+                binding.labelRes.text = "R$ ${"%.2f".format(totalValue)}"
+            }
+
+        } else {
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
